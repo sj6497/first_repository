24052236e75645e97964785b957efa09381c8082
sj6497 <57174526+sj6497@users.noreply.github.com>
2019-10-30 22:10:11 +0530

    Create transpose.c

---
+++ transpose.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+int row,col;
+int i,j;
+scanf("%d %d",&row,&col);
+int arr[row][col];
+for(i=0;i<row;i++){
+    for(j=0;j<col;j++){
+        scanf("%d 3%d",&arr[i][j]);
+    }
+}
+
+for(i=0;i<row;i++){
+    for(j=0;j<col;j++){
+        if(i>j){
+            int t=arr[i][j];
+            arr[i][j]=arr[j][i];
+            arr[i][j]=t;
+        }
+    }
+}
+
+for(i=0;i<row;i++){
+    for(j=0;j<col;j++){
+        printf("%d\n",arr[i][j]);
+    }
+}
+
+}
